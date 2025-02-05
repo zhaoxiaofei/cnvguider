08eba8a316ee541ea98e71c56d7d48d67ec01cc9
ZhaoXiaofei <cndfeifei@hotmail.com>
2025-02-06 01:50:34 +0800

    Fixed ValueError: invalid mode: 'no_overwritting'

--- common.py
+++ common.py
@@ -98,6 +98,8 @@ def myopen(filename, mode):
     if mode in OVERWRITING_PREVENTION_MODES and os.path.exists(filename):
         logging.info(F'Writing to {filename} is redirected to {os.devnull} to prevent overwriting')
         return open(os.devnull, 'w')
+    elif mode in OVERWRITING_PREVENTION_MODES:
+        return open(filename, 'w')
     else:
         return open(filename, mode)
 
